ddba7120309ba58a1b29a2230fc5716bfb608f22
Suruch Chakrapeesirisuk <suruch.c@bitazza.com>
2024-03-20 02:57:28 +0700

    Added btn clinic search and logout

--- healthcare/lib/feature/dashboard/presentation/dashboard.dart
+++ healthcare/lib/feature/dashboard/presentation/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare/feature/landingpage/presentation/landingpage.dart';
 import 'package:healthcare/model/customermodel.dart';
 
 class DashBoard extends StatefulWidget {
@@ -13,6 +14,20 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  void _logOut() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const LandingPage(),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -24,24 +39,80 @@ class _DashBoardState extends State<DashBoard> {
           child: Column(
             children: [
               Container(
-                width: screenWidth,
-                padding: const EdgeInsets.only(
-                  top: 90,
-                  left: 20,
-                  right: 20,
-                  bottom: 70,
-                ),
-                color: Colors.green,
-                child: Text(
-                  "Hello, ${widget.customer.firstName} ${widget.customer.lastName}",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.grey[850],
-                    fontWeight: FontWeight.bold,
-                    overflow: TextOverflow.ellipsis,
+                  width: screenWidth,
+                  padding: const EdgeInsets.only(
+                    top: 90,
+                    left: 10,
+                    right: 10,
+                    bottom: 70,
+                  ),
+                  color: Colors.green,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 8,
+                        child: Text(
+                          "Hello, ${widget.customer.firstName} ${widget.customer.lastName}",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey[850],
+                            fontWeight: FontWeight.bold,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: IconButton(
+                            onPressed: () => _logOut(),
+                            icon: Icon(
+                              Icons.logout_outlined,
+                              color: Colors.grey[900],
+                              size: 30.0,
+                            ),
+                          ))
+                    ],
+                  )),
+              const SizedBox(height: 40),
+              TextButton(
+                onPressed: null,
+                style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    minimumSize: const Size(50, 1),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    alignment: Alignment.centerLeft),
+                child: Container(
+                  width: screenWidth,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                      image: const AssetImage("assets/search_bg.png"),
+                      fit: BoxFit.cover,
+                    ),
+                    color: Colors.greenAccent,
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  padding: const EdgeInsets.only(
+                    top: 40,
+                    left: 20,
+                    right: 20,
+                    bottom: 40,
+                  ),
+                  margin: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: Text(
+                    "Clinic Near You",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.teal[900],
+                    ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
