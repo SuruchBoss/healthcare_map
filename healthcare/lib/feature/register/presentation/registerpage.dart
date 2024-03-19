3549001566972a258b36a44fe6b0543504104c52
Suruch Chakrapeesirisuk <suruch.c@bitazza.com>
2024-03-20 00:26:18 +0700

    Create register screen

--- healthcare/lib/feature/register/presentation/registerpage.dart
+++ healthcare/lib/feature/register/presentation/registerpage.dart
@@ -9,9 +9,34 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final nameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  int? selectedAge;
+  final heighController = TextEditingController();
+  final weightController = TextEditingController();
+
+  List<DropdownMenuItem<int>> ageItems = List.generate(91, (int index) {
+    return DropdownMenuItem(
+      value: index,
+      child: Text('$index'),
+    );
+  });
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void _handleRegistration() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    Map<String, dynamic> data = {
+      "name": "Suruch",
+      "age": 20,
+      "subject": "Math",
+    };
+
+    await firestore.collection("Students").add(data);
   }
 
   @override
@@ -19,35 +44,206 @@ class _RegisterPageState extends State<RegisterPage> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.only(
-          top: 50,
-          left: 40,
-          right: 40,
-          bottom: 30,
-        ),
-        width: screenWidth,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextButton(
-                onPressed: () async {
-                  FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-                  Map<String, dynamic> data = {
-                    "name": "Suruch",
-                    "age": 20,
-                    "subject": "Math",
-                  };
-
-                  await firestore.collection("Students").add(data);
-                },
-                child: Text("Click"),
-              )
-            ],
+      body: Stack(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            color: Colors.white,
+            padding: const EdgeInsets.only(
+              top: 40,
+              left: 20,
+              right: 20,
+              bottom: 30,
+            ),
+            width: screenWidth,
+            child: Container(
+              width: 400,
+              height: 600,
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 10,
+                right: 10,
+                bottom: 20,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.blueAccent, width: 3),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: nameController,
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'First Name',
+                        hintText: 'First Name',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: lastNameController,
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Last Name',
+                        hintText: 'Last Name',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Age: ",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 70,
+                        child: DropdownButton<int>(
+                          isExpanded: true,
+                          value: selectedAge,
+                          onChanged: (newValue) {
+                            setState(() {
+                              selectedAge = newValue;
+                            });
+                          },
+                          items: ageItems,
+                          underline: Container(
+                            height: 2,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Weight: ",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 220,
+                        child: TextField(
+                          controller: weightController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Weight',
+                            hintText: 'Weight',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Height: ",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 220,
+                        child: TextField(
+                          controller: heighController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Heigh',
+                            hintText: 'Heigh',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 110),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextButton(
+                        onPressed: () => null,
+                        style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            minimumSize: const Size(50, 30),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            alignment: Alignment.centerLeft),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 21,
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () => null,
+                        style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            minimumSize: const Size(50, 30),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            alignment: Alignment.centerLeft),
+                        child: const Text(
+                          "Clear",
+                          style: TextStyle(
+                            fontSize: 21,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60,
+              left: 20,
+              right: 20,
+              bottom: 30,
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+                size: 30.0,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
