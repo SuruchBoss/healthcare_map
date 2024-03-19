2d0ef36375807e59baa2b91da1f21d420124b3b4
Suruch Chakrapeesirisuk <suruch.c@bitazza.com>
2024-03-19 20:11:39 +0700

    Firebase initiate

--- healthcare/lib/main.dart
+++ healthcare/lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:healthcare/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
